310ddd5aaa46d27942adfe9e8a80fc0b673dd014
maryam zarei <maryamzarei9319@gmail.com>
2024-05-23 13:53:38 +0330

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+export { default as ChannelCard } from '../components/channelcard';
+export { default as VideoCard } from '../components/videocard';
+export { default as ChannelDetail } from './ChannelDetail';
+export { default as Feed } from '../components/Feed';
+export { default as Loader } from '../components/loadbar';
+export { default as Navbar } from '../components/Navbar';
+export { default as SearchBar } from '../components/searchbar';
+export { default as VideoDetail } from './VideoDetail';
+export { default as SearchFeed } from './SearchFeed';
+export { default as Videos } from '../components/videos';
+export { default as Sidebar } from '../components/sidebar';
